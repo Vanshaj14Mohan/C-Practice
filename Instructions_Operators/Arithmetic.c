e5983f4c1480d1b5759db0357640f02eeebcafe9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-11 15:27:51 +0530

    test case

---
+++ Instructions_Operators/Arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+  int b = 2;
+  int c = 4;
+//   printf("Enter b value");
+//   scanf("%d", &b);
+//   printf("Enter c value");
+//   scanf("%d", &c);
+  int power = pow(b,c);
+  printf("Power function: %d", power);
+  return 0;
+}
