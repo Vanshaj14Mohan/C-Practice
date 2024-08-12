11e8bea76e41026971456b9102a24f3f805a6bc2
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-12 08:46:33 +0530

    Operators

--- Instructions_Operators/Arithmetic.c
+++ Instructions_Operators/Arithmetic.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<math.h>
+#include<math.h> //math.header file for maths related problems
 
+//for checking power function
 int main(){
-  int b = 2;
-  int c = 4;
-//   printf("Enter b value");
-//   scanf("%d", &b);
-//   printf("Enter c value");
-//   scanf("%d", &c);
+  int b,c;
+  printf("Enter b value");
+  scanf("%d", &b);
+  printf("Enter c value");
+  scanf("%d", &c);
   int power = pow(b,c);
   printf("Power function: %d", power);
   return 0;
---
+++ Instructions_Operators/modulo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int b =5;
+    int c = 2;
+    int modulo = b%c;
+    printf("Modular operation: %d", modulo);
+    return 0;
+}
