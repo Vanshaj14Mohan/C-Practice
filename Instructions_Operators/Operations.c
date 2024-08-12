34193245a48568e523adbf6dd00e767ca2768c97
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-12 23:37:43 +0530

    operations

--- Instructions_Operators/Operations.c
+++ Instructions_Operators/Operations.c
@@ -9,9 +9,10 @@ int main()
     int sum = a+b;
     int div = a-b;
     int mul = a*b;
-    // int div = a-b;
+    int divslash = a/b;
     printf("Sum of number is: %d \n", sum);
     printf("Divison of number is: %d \n", div);
     printf("Muliply of number is: %d \n", mul);
+    printf("By of number is: %d \n", divslash);
     return 0;
 }
