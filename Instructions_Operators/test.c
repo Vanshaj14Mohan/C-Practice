4507750394c2884a76838b627947a5929f475e72
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-15 23:28:48 +0530

    files

--- Instructions_Operators/test.c
+++ Instructions_Operators/test.c
@@ -7,11 +7,13 @@ int main(){
     int y = 5*2/2*3;
     int z = 5+2/2*3;
     int w = 5*(2/2)*3;
-    printf("%d \n", a);
-    printf("%d \n", b);
-    printf("%d \n", x);
-    printf("%d \n", y);
-    printf("%d \n", z);
-    printf("%d \n", w);
+    printf("For a: %d \n", a);
+    printf("For b: %d \n", b);
+    printf("For x: %d \n", x);
+    printf("For y: %d \n", y);
+    printf("For z: %d \n", z);
+    printf("For w: %d \n", w);
+    printf("%d \n", 4>3); //will show 1 as true
+    printf("%d \n", 3>4); //will show 0 as false
     return 0;
 }  
