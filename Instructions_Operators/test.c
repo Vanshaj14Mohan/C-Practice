d39a67d3a194e3e77af78e0228d52e5b5e838fc8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-15 17:47:06 +0530

    test files

---
+++ Instructions_Operators/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a = 1.99999;
+    int b = 4*3/6*2;
+    int x = 5*2 - 2*3;
+    int y = 5*2/2*3;
+    int z = 5+2/2*3;
+    int w = 5*(2/2)*3;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", x);
+    printf("%d \n", y);
+    printf("%d \n", z);
+    printf("%d \n", w);
+    return 0;
+}  
