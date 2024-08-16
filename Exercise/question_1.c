c4b17622ff05126f327c7cb5e19a78b05ea02393
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-16 23:11:21 +0530

    Questions

---
+++ Exercise/question_1.c
@@ -0,0 +1,16 @@
+//Wap to check if a number is divisible by 2 or not.
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    //printf("%d", n%2 ==0); short trick
+    if(n %2 == 0){
+        printf("Divisible by 2");
+    }
+    else{
+        printf("Not Divisible");
+    }
+    return 0;
+}
