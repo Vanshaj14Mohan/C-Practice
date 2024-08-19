b78bb65a5c1a471659a456c310301281471a3808
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-19 19:42:19 +0530

    question

---
+++ Conditional_statements/ques.c
@@ -0,0 +1,17 @@
+//Wap to check if a student passed or failed.
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("Enter the marks out of 100: ");
+    scanf("%d", &marks);
+    if( marks>30){
+        printf("Student pass");
+    }
+    else if (marks <= 30){
+        printf("Student Fail");
+    }
+    else{
+        printf("Enter valid number");
+    }
+    return 0;
+}
