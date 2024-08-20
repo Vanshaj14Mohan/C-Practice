09483ebd9396dd52fa8760dc0772d3c1c00e8102
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-21 00:37:28 +0530

    question

--- Conditional_statements/ques_3.c
+++ Conditional_statements/ques_3.c
@@ -1,17 +1,25 @@
 //Wap to find if a character entered by user is uppercase or not.
 #include<stdio.h>
+#include<ctype.h>
 int main(){
     char ch;
     printf("Enter a character: ");
     scanf("%c", &ch);
-    if(ch >= 'A' && ch <='Z'){
-        printf("Uppercase");
-    }
-    else if(ch >= 'a' && ch >= 'z'){
-        printf("Lowercase");
+    // if(ch >= 'A' && ch <='Z'){
+    //     printf("Uppercase");
+    // }
+    // else if(ch >= 'a' && ch >= 'z'){
+    //     printf("Lowercase");
+    // }
+    // else{
+    //     printf("Not official letter");
+    // }
+    //another method
+    if(isupper(ch)){
+        printf("Character is in Uppercase");
     }
     else{
-        printf("Not official letter");
+        printf("Character is not in Uppercase");
     }
     return 0;
 }
