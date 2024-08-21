6619c0faad32464171dc662c9e6c403e00d1ca2b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-21 21:49:49 +0530

    operators

--- Instructions_Operators/oper.c
+++ Instructions_Operators/oper.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
     //increment operator
-    //i++, ++i
+    //i++, (pre increment)
+    //++i, (post increment)
 
     int i =1;
     printf("%d \n", i++); //1
--- Loops/demo.c
+++ Loops/demo.c
@@ -4,6 +4,12 @@ int main(){
     for(i =0; i<10; i++){
         printf("Helloworld \n");
     }
+    for(float i = 0; i<=5; i++){
+        printf("%f \n", i);
+    }
+    for(char ch ='a'; ch<='z'; ch++){
+        printf("%c\n", ch);
+    }
     return 0;
 }
 // #include<stdio.h>
