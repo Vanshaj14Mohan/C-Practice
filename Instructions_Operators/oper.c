c317e4e2966072849109c2db387ece2baf7a318b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-21 21:19:26 +0530

    operators

---
+++ Instructions_Operators/oper.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //increment operator
+    //i++, ++i
+
+    int i =1;
+    printf("%d \n", i++); //1
+    printf("%d \n", i); //2
+    int y = 2;
+    printf("%d \n", ++y); //3
+    printf("%d \n", y); //3
+}
