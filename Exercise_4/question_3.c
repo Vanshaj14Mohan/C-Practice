5776edee1a0e32adcf39c3954c20c93cd37bee07
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-03 22:28:47 +0530

    questions

--- Exercise_4/question_3.c
+++ Exercise_4/question_3.c
@@ -1,5 +1,22 @@
 //Wap to print prime numbers in a range.
 #include<stdio.h>
 int main(){
-    int i;
+    int start, end, count;
+    //taking input for range
+    printf("Enter starting value: ");
+    scanf("%d", &start);
+    printf("Enter ending value: ");
+    scanf("%d", &end);
+    for(int i =start; i<=end; i++){
+        count=0;
+        for(int j = 1; j<=i;j++){
+            if(i%j == 0){
+                count = count+1;
+            }
+        }
+        if(count ==2){
+            printf("Prime number %d \n", i);
+        }
+    }
+    return 0;
 }
