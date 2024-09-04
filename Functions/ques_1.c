2355d24d14118f975e8c5ca1e8c2123864f64048
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-04 23:48:09 +0530

    Functions

---
+++ Functions/ques_1.c
@@ -0,0 +1,31 @@
+//Wap a function to print "Hello" and second to print "good byee".
+// #include<stdio.h>
+
+// void printMsg(); //declaration prototype
+
+// int main(){
+//     printMsg(); //function call
+// }
+
+// void printMsg(){ //function definition
+//     printf("Hello\n"); 
+//     printf("good byee\n");
+// }
+
+//or method 2
+#include<stdio.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+    printHello();
+    printGoodbye();
+}
+
+void printHello(){
+    printf("Hello!\n");
+}
+
+void printGoodbye(){
+    printf("Good byee :)\n");
+}
