b7e4ba7bc5ea42c979439f677a8b8832ca49c2c8
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-14 23:39:21 +0530

    files

---
+++ Functions/ques_5.c
@@ -0,0 +1,17 @@
+//Wap to count gst on a given price
+#include<stdio.h>
+
+void calcPrice( float n); //function declarartion
+
+int main(){
+    float n;
+    printf("Enter the number:");
+    scanf("%f", &n);
+    calcPrice(float n);
+    return 0;
+}
+
+void calcPrice(float n){
+    n = n + (0.18*100);
+    printf("New amount after gst is : %f", n);
+}
