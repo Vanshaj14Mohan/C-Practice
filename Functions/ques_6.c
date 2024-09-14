40304a90bad683a02b8e8ac5f8236032de8afe01
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-15 00:34:53 +0530

    files

---
+++ Functions/ques_6.c
@@ -0,0 +1,11 @@
+//Wap to calculate the square of a number given by the user
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int n = 4;
+    printf("%f", pow(4,2)); //f coz pow is double so to convert int to double
+    return 0;
+}
+
+//Note-> Whenever we want to print double values use float
