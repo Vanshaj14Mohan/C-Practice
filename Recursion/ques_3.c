88261a777291b6ac4501bcb6503d1d2b963ad4ac
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-24 23:29:45 +0530

    files

---
+++ Recursion/ques_3.c
@@ -0,0 +1,15 @@
+// Write a function to convert celsius to fahrenheit
+#include<stdio.h>
+float convTemp(float celsius);
+
+int main(){
+    float far = convTemp(10);
+    printf("Fahrenheit: %f", &far);
+    return 0;
+}
+
+//recursive function
+float convTemp(float celsius){
+    float far = celsius *(9/5)+32;
+    return far;
+}
