adc3fc909d4ffa99b423740a5f479c6c7907933c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-09-26 00:25:44 +0530

    files

---
+++ Recursion/ques_4.c
@@ -0,0 +1,17 @@
+//Write a function to calculate Percentage of a student for marks in Science, Maths and Sanskrit.
+#include<stdio.h>
+
+int perCounter(int Science, int Maths, int Sanskrit);
+
+int main(){
+    int science = 92;
+    int maths = 94;
+    int sanskrit = 91;
+    printf("Percentage is: %d", perCounter(science, maths,sanskrit));
+    return 0;
+}
+
+//Recursive function
+int perCounter(int Science, int Maths, int Sanskrit){
+    return ((Science + Maths + Sanskrit)/3);
+}
