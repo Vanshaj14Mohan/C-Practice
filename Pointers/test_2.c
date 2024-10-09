ee35f9da3624f0fd76b97c72cea67091943ad1c9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-09 23:33:12 +0530

    files

--- Pointers/test_2.c
+++ Pointers/test_2.c
@@ -1,9 +1,13 @@
 //Pointer to Pointer case:
 #include<stdio.h>
 int main(){
-    float price = 100.0;
-    float *ptr = &price;
-    float **pptr = &ptr;
-    printf("Value stored in above is", **pptr);
+    // float price = 100.0;
+    // float *ptr = &price;
+    // float **pptr = &ptr;
+    // printf("Value stored in above is", **pptr);
+    int i = 5;
+    int *ptr = &i;
+    int **pptr = &ptr;
+    printf("Value at **pptr is: %d\n", **pptr);
     return 0;
 }
