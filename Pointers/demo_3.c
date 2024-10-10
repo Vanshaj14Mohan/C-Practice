b006b34d03e2aecf0f1ec0a17adf50455b5df129
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-10 23:51:40 +0530

    files

---
+++ Pointers/demo_3.c
@@ -0,0 +1,17 @@
+//Call by value test
+#include<stdio.h>
+
+void square(int n);
+
+int main(){
+    int number = 4;
+    square(number);
+    printf("Number = %d\n", number);
+    return 0;
+}
+
+//call by value
+void square(int n){
+    n = n*n;
+    printf("Square is %d\n:", n);
+}
