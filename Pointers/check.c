c1e844de70672a8a0c1d207325df41944983899c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-12 23:58:17 +0530

    files

--- Pointers/check.c
+++ Pointers/check.c
@@ -1,1 +1,15 @@
 //Check the ans
+#include<stdio.h>
+
+void printAddress(int n);
+
+int main(){
+    int n = 4;
+    printf("%p\n", &n);
+    printAddress(n);
+    return 0;
+}
+
+void printAddress(int n){
+    printf("%p\n", &n);
+}
