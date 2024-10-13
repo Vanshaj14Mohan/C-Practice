d3bea9e7029035e692d5c4b5e8163b848233a3d5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-13 15:48:52 +0530

    files

---
+++ Pointers/demo_6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void testCase(int a, int b, int *sum, int *prod, int *avg);
+
+int main(){
+    int a , b;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    int sum, prod, avg;
+    testCase(a, b, &sum, &prod, &avg);
+
+    printf("sum = %d, prod = %d, avg = %d\n", sum, prod, avg);
+    return 0;
+}
+
+void testCase(int a, int b, int *sum, int *prod, int *avg ){
+//using pointer
+*sum = a+b;
+*prod = a*b;
+*avg = (a+b)/2;
+}
