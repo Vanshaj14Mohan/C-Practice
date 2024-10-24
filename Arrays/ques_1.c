09b8a5e8a6a5221b5b7cc32df3352bc03fed158d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-24 23:25:53 +0530

    files

--- Arrays/ques_1.c
+++ Arrays/ques_1.c
@@ -1,1 +1,14 @@
 //Wap to enter the price of three items and print their final cost with gst.
+#include<stdio.h>
+int main(){
+    float price[3];
+    printf("Enter prices of three products: ");
+    scanf("%f", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+
+    printf("Price of first product is: %f\n ", price[0]+(0.18*price[0]));
+    printf("Price of second product is: %f\n ", price[1]+(0.18*price[1]));
+    printf("Price of third product is: %f\n", price[2]+(0.18*price[2]));
+    return 0;
+}
