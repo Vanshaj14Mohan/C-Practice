c650d25863439b70ce4a86c5e21614a17dda5800
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-31 00:33:43 +0530

    files

--- Arrays/demo_4.c
+++ Arrays/demo_4.c
@@ -4,4 +4,7 @@ int main(){
     int _age = 23;
     int *ptr = &age;
     int *_ptr = &_age;
+    printf("%u, %u Difference = %u\n", ptr, _ptr, ptr-_ptr);
+    printf("Comparison = %u\n", ptr == _ptr);
+    return 0;
 }
