a5b6a8d36dc714d637a8f1ea87f6127296bd6580
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-10-30 13:58:32 +0530

    files

---
+++ Arrays/demo_2.c
@@ -0,0 +1,12 @@
+//Pointer arithmetic, increment and decrement operator 
+#include<stdio.h>
+int main(){
+    int age = 22;
+    int *ptr = &age;
+    printf("ptr = %u\n", ptr);
+    ptr++;
+    printf("ptr = %u\n", ptr);
+    ptr--;
+    printf("ptr = %u", ptr);
+    return 0;
+}  
