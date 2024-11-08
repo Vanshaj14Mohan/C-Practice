02c7fca0b723a8f0aa872e87262176d509f5819d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-08 22:59:53 +0530

    files

--- Arrays/2D Arrays/test_2.c
+++ Arrays/2D Arrays/test_2.c
@@ -3,7 +3,7 @@
 int countOdd(int arr[], int n); //function prototype
 int main(){
     int arr[] = {1,2,3,4,5,6};
-    printf("%d", countOdd(arr, 6));
+    printf("Total odd numbers are: %d", countOdd(arr, 6));
     return 0;
 }
 
