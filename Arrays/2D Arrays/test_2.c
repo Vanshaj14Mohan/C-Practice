bea0e366712c61fc921d996ec232f672b77b3675
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-08 23:08:33 +0530

    files

--- Arrays/2D Arrays/test_2.c
+++ Arrays/2D Arrays/test_2.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+//Write a function to count the number of odd numbers in an array.
 
+#include<stdio.h>
 int countOdd(int arr[], int n); //function prototype
 int main(){
     int arr[] = {1,2,3,4,5,6};
-    printf("Total odd numbers are: %d", countOdd(arr, 6));
+    printf("Total odd numbers are: %d", countOdd(arr, 6)); //function call
     return 0;
 }
 
-int countOdd(int arr[], int n){
+int countOdd(int arr[], int n){ //function definition 
     int count = 0;
     for(int i =0; i<n; i++){
         if(arr[i] %2 !=0){
