57ecb5bcf357bbe1640cb828bdc83d306b3af90f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-11 00:22:33 +0530

    questions

--- Arrays/2D Arrays/demo_3.c
+++ Arrays/2D Arrays/demo_3.c
@@ -1,1 +1,35 @@
-////Write a function to reverse an array.
+//Write a function to reverse an array.
+#include<stdio.h>
+//function prototype
+
+//now to print array
+void printArr(int arr[], int n);
+
+void revArr(int arr[], int n); //function prototype
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    revArr(arr, 6); //function call
+    printArr(arr, 6);
+    return 0;
+}
+
+void printArr(int arr[], int n){
+    for(int i =0; i<n; i++){
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+
+void revArr(int arr[], int n){ //function definition
+    for(int i =0; i<n/2; i++){
+       int firVal = arr[i];
+       int secVal =  arr[n-i-1]; //classic formula
+       //now to reverse it
+       arr[i] = secVal;
+       arr[n-i-1] = firVal;
+
+    }
+
+}
