50663754eab652bc5b937a25275f81c53db76432
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-10 23:42:21 +0530

    files

--- Arrays/2D Arrays/demo.c
+++ Arrays/2D Arrays/demo.c
@@ -1,29 +1,3 @@
-// #include <stdio.h>
-
-// int main() {
-//     // Define a 2D array (3x3 array)
-//     int arr[3][3] = {
-//         {1, 2, 3},
-//         {4, 5, 6},
-//         {7, 8, 9}
-//     };
-
-//     // Get the number of rows and columns
-//     int rows = 3;
-//     int cols = 3;
-
-//     // Nested loop to print the 2D array
-//     for (int i = 0; i < rows; i++) {
-//         for (int j = 0; j < cols; j++) {
-//             printf("%d ", arr[i][j]);
-//         }
-//         // Newline after each row
-//         printf("\n");
-//     }
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 int main() {
