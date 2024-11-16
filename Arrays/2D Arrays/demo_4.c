a518867b0c8d9e3468e1c4190341cb88c58588ec
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-16 23:05:13 +0530

    files

--- Arrays/2D Arrays/demo_4.c
+++ Arrays/2D Arrays/demo_4.c
@@ -1,18 +1,16 @@
 //Write a program to store the first n fibonaaci numbers.
 #include<stdio.h>
 int main(){
-int n;
-printf("Enter n:");
-scanf("%d", &n);
-int fib_arr[n];
-fib_arr[0] = 0;
-fib_arr[1] = 1;
-for(int i = 2; i < n; i++){
-fib_arr[i] = fib_arr[i-1] + fib_arr[i-2];
-}
-printf("Fibonacci series: ");
-for(int i = 0; i < n; i++){
-printf("%d ", fib_arr[i]);
-}
-return 0;
+    int n;
+    printf("Enter n (n>2): ");
+    scanf("%d", &n);
+    int fib_arr[100];
+    fib_arr[0] = 0;
+    fib_arr[1] = 1;
+    for(int i = 2; i<n; i++){ //1,2,3,5...
+        fib_arr[i] = fib_arr[i-1] + fib_arr[i-2]; //baisc formula nth therm = (n-1) + (n-2)
+        printf("%d\t", fib_arr[i]);
+    }
+    printf("\n");
+    return 0;
 }
