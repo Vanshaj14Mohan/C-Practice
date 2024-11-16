4e2ed833df58ea836e38a655ecd7f1daaf334ebe
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-16 22:40:20 +0530

    files

--- Arrays/2D Arrays/demo_4.c
+++ Arrays/2D Arrays/demo_4.c
@@ -1,1 +1,18 @@
 //Write a program to store the first n fibonaaci numbers.
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter n:");
+scanf("%d", &n);
+int fib_arr[n];
+fib_arr[0] = 0;
+fib_arr[1] = 1;
+for(int i = 2; i < n; i++){
+fib_arr[i] = fib_arr[i-1] + fib_arr[i-2];
+}
+printf("Fibonacci series: ");
+for(int i = 0; i < n; i++){
+printf("%d ", fib_arr[i]);
+}
+return 0;
+}
