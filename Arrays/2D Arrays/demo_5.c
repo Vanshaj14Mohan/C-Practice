48e87347881137e21218a451542b1d10b6901831
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-21 23:26:15 +0530

    files

---
+++ Arrays/2D Arrays/demo_5.c
@@ -0,0 +1,13 @@
+//Create 2D arrays, storing the tables of 2 and 3.
+#include<stdio.h>
+
+void storeTable(int arr[][], int n, int m);
+
+int main(){
+    int tables[2][10];
+    return 0;
+}
+
+void storeTable(int arr[][], int n, int m){
+
+}
