d3f286fdd4d28484fb7d07519a13c8fb550d3c8c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-23 23:41:30 +0530

    files

--- Arrays/demo_6.c
+++ Arrays/demo_6.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-void storeTable(int arr[][10], int n, int m);
+void storeTable(int arr[][10], int n, int m, int number);
 int main(){
     int tables[2][10];
+    storeTable(tables,0,10,2);
+    storeTable(tables,1,10,3);
+    return 0;
 }
 
-void storeTable(int arr[][10], int n, int m, ){
-    for(int i =0; i<m; i++){
-        for(int j=0; j<m; j++){
-
-        }
+void storeTable(int arr[][10], int n, int m, int number){
+    for(int i =0; i<m; i++){ //0 to 10
+        arr[n][m] = number *(i + 1);
 
     }
 }
