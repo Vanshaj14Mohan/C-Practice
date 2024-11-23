8ee3713418d551af25c917a67d5725f56addc967
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-23 23:28:15 +0530

    files

---
+++ Arrays/demo_6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void storeTable(int arr[][10], int n, int m);
+int main(){
+    int tables[2][10];
+}
+
+void storeTable(int arr[][10], int n, int m, ){
+    for(int i =0; i<m; i++){
+        for(int j=0; j<m; j++){
+
+        }
+
+    }
+}
