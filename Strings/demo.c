dd9675521925be611b4131ec5e868493a98e5dec
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-29 23:54:58 +0530

    files

--- Strings/demo.c
+++ Strings/demo.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
     //char name[] = {'A', 'M', 'A', 'N', '\0'};
+    //another way to implement
     char name[] = "Aman";
     char class[] = "A good day";
     printf("Name is: %s\n", name);
