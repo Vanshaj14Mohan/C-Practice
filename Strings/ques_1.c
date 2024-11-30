c91bfa78f74d5e7d09a8481c593abace95d3b9b7
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-11-30 23:54:43 +0530

    files

--- Strings/ques_1.c
+++ Strings/ques_1.c
@@ -1,1 +1,19 @@
 //Create a String firstName & lastName to store details of user and print all the characters using loop.
+#include<stdio.h>
+
+void printString(char arr[]);
+
+int main(){
+    char firstName[] = "Aman";
+    char lastName[] = "Mathur";
+    printString(firstName);
+    printString(lastName);
+    return 0;
+}
+
+void printString(char arr[]){
+    for(int i =0; arr[i] !='\0'; i++){
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+}
