d1a75eee94f268678a74f9477750308469dc71f9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-02 23:26:12 +0530

    files

---
+++ Strings/ques_2.c
@@ -0,0 +1,28 @@
+//Ask the user to enter their firstName and print it back to them. Also try this with their fullName
+// #include<stdio.h>
+// int main(){
+//     // char firstName[10];
+//     // scanf("%s", firstName);
+//     // printf("Given FirstName is %s: ", firstName);
+//     char fullName[30];
+//     scanf("%s", fullName);
+//     printf("Full Name is %s", fullName);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     char name[100];
+//     gets(name);
+//     puts(name);
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    char name[100];
+    fgets(name, 100, stdin);
+    puts(name);
+    return 0;
+}
+
