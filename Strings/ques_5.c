0c1824c8d02a89942fea59cd231ca22598e284ce
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-21 22:27:51 +0530

    files

---
+++ Strings/ques_5.c
@@ -0,0 +1,8 @@
+//Find the salted form of a password entered by the user if the salt is "123" & add it at the end.
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char password[100];
+    scanf("%s", password);
+}
