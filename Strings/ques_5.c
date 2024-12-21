3f00069e104de4b5a69d9f62ba169e89ed061a57
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-21 22:57:05 +0530

    files

--- Strings/ques_5.c
+++ Strings/ques_5.c
@@ -7,4 +7,16 @@ void salting(char password[]); //function to
 int main(){
     char password[100];
     scanf("%s", password);
+    salting(password);
+    return 0;
+}
+
+void salting(char password[]){
+    char salt[] = "123";
+    char newPass[200];
+
+    //using library functions
+    strcpy(newPass, password); //newPass = test
+    strcat(newPass, salt);  //newPass = "test" + "123"
+    puts(newPass);
 }
